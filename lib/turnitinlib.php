1a5ce01c786ffcfaa338ed58145ef199ccb9d79f
Dan Marsden <dan@catalyst.net.nz>
2009-05-25 15:01:45 +1200

    Turnitin: add boilerplate/phpdoc stuff

--- lib/turnitinlib.php
+++ lib/turnitinlib.php
@@ -1,9 +1,27 @@
 <?php
-/**
-* Turnitin Library
-* v 1.0 2007/04/23 10:00:00 Dan Marsden
-*/
 
+// This file is part of Moodle - http://moodle.org/ 
+// 
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * turnitinlib.php - Contains Turnitin related functions called by Modules.
+ * 
+ * @author    Dan Marsden <dan@danmarsden.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
 function tii_get_url($tii, $returnArray=false) {
     global $CFG;
     $tiisettings = get_records_menu('config_plugins', 'plugin', 'tii', '', 'name,value');
